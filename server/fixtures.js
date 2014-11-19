c843afb6ad9d4407194cc3d0ee9ff719380ed8b1
Raissa Largman <raissa.largman@gmail.com>
2014-11-19 00:06:33 -0800

    Trip details template and activities card templates

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,12 +3,50 @@ var fixture_destinations = [
   { 
     name: "3 Days on the Dalmatian Coast",
     location: "Split, Croatia",
-    imageSrc: "earthporn.jpg"
+    description: "Here is some nonsense about Croatia. This is about two \
+      sentences long. Perhaps three.",
+    imageSrc: "earthporn.jpg",
+    activities: [
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      },
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      },
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      }
+    ]
   },
   {
     name: "A week in the Andes",
     location: "Chile",
-    imageSrc: "earthporn.jpg"
+    description: "Here is some nonsense about Chile. This is about two \
+      sentences long. Perhaps three.",
+    imageSrc: "earthporn.jpg",
+    activities: [
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      },
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      },
+      {
+        name: "Hike this trail",
+        description: "Outdoors stuff is awesome, yay. Do it all the time if \
+          you like nature things and being outside."
+      }
+    ]
   }
 ];
 
