8e2cd21b0c76260d50b75f26862b78a221f491b8
Moustafa <masfour@stanford.edu>
2014-11-20 00:28:18 -0800

    Added first 5 destinations to fixtures.js

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,53 +1,331 @@
 // Fixture data. Refer to destinations.js for schema
 var fixture_destinations = [
+
+  // destination 1
   { 
-    name: "3 Days on the Dalmatian Coast",
-    location: "Split, Croatia",
-    description: "Here is some nonsense about Croatia. This is about two \
-      sentences long. Perhaps three.",
-    imageSrc: "earthporn.jpg",
+    name: "Galapagos, Ecuador",
+    continent: "South America",
+    description: "A must explore for outdoor enthusiasts and animal lovers, the Galapagos are an epic adventure.",
+    lodging: "Hostal Loja",
+    imageSrc: "galapagos.jpg",
+
     activities: [
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Las Loberias Beach",
+        description: "Spend an afternoon with the Sea Lions on this beach of San Cristobal island. Make sure not to get too close!",
+        imageSrc: "sealions.jpg"
       },
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Reserva El Chato",
+        description: "Hike through this nature preserve to see giant tortoises in their natural habitat.";
+        imageSrc: "Tortoise.jpg"
       },
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Kicker Rock",
+        description: "Snorkel or dive with sting rays and land sharks. Book a tour with Ecuagringo Adventures.",
+        imageSrc: "kickerrock.jpg"
       }
     ]
   },
-  {
-    name: "A week in the Andes",
-    location: "Chile",
-    description: "Here is some nonsense about Chile. This is about two \
-      sentences long. Perhaps three.",
-    imageSrc: "earthporn.jpg",
+
+  // destination 2
+  { 
+    name: "Rio de Janeiro, Brazil",
+    continent: "South America",
+    description: "Rio offers the best of all worlds, some awesome outdoor spaces and enchanting culture.",
+    lodging: "Cabana Copa Hostel",
+    imageSrc: "rio.jpg",
+
     activities: [
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Sugar Loaf Mountain",
+        description: "Get an amazing view of the city from the top of Sugar Loaf Mountain. Earn it by hiking from the beach, or take a cable car for $10.",
+        imageSrc: "sugarloaf.jpg"
       },
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Cristo Redentor",
+        description: "Get there early to miss the crowds at this must-do in Rio. Take the trolley up for more awesome views.",
+        imageSrc: "Cristo.jpg"
       },
       {
-        name: "Hike this trail",
-        description: "Outdoors stuff is awesome, yay. Do it all the time if \
-          you like nature things and being outside."
+        name: "Pedra do Sal",
+        description: "Enjoy a Caipirinha in this funky neighborhood north of the city. The open air bar with live music makes a great night out.",
+        imageSrc: "pedradesal.jpg"
       }
     ]
-  }
+  },
+
+  // destination 3
+  { 
+    name: "Barcelona",
+    continent: "Europe",
+    description: "See awesome art & architecture, and party with Europe’s craziest in one of the world’s most unique cities.",
+    lodging: "Hola Hostel",
+    imageSrc: "barcelona.jpg",
+
+    activities: [
+      {
+        name: "Park Güell",
+        description: "Stroll through Gaudi’s park to appreciate the beautiful gardens and structures. Entrance is free.",
+        imageSrc: "Park-Guell.jpg"
+      },
+      {
+        name: "Picasso Museum",
+        description: "The museum is home to the most extensive collection of Picasso’s work and is more than worth the cost of admission. Make sure to check out the collection on Las Meninas.",
+        imageSrc: "Picasso.jpg"
+      },
+      {
+        name: "Opium Cub",
+        description: "Check out Barcelona’s nightlife at one of the most popular clubs. Snack on churros con chocolate on the beach out front until metro opens at 5am for a true Barcelona night.",
+        imageSrc: "opium.jpg"
+      }
+    ]
+  },
+
+  // destination 4
+  { 
+    name: "Florence, Italy",
+    continent: "Europe",
+    description: " A must in your lifetime, Florence has both natural beauty and some of the world’s best art & architecture.",
+    lodging: "Hotel Benvenuti",
+    imageSrc: "florence.jpg",
+
+    activities: [
+      {
+        name: "Enjoy the Best Gelato",
+        description: "Almost every block has a gelateria, and they’re all delicious. Be adventurous and try some wacky flavors.",
+        imageSrc: "gelato.jpg"
+      },
+      {
+        name: "Bike Tuscany",
+        description: "Spend a day biking through the beautiful tuscany landscape, and stop for a few tastings at wineries. Rent bikes from Mike’s Bikes.",
+        imageSrc: "tuscany.jpg"
+      },
+      {
+        name: "Michelangelo’s David",
+        description: "Its crowded, but you can’t miss David and the Galleria dell’Accademia. Buy tickets in advance online.",
+        imageSrc: "david.jpg"
+      }
+    ]
+  },
+
+  // destination 5
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 6
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 7
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 8
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 9
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 10
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 11
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+  // destination 12
+  { 
+    name: "",
+    continent: "",
+    description: "",
+    lodging: "",
+    imageSrc: "",
+
+    activities: [
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      },
+      {
+        name: "",
+        description: "",
+        imageSrc: ""
+      }
+    ]
+  },
+
+// end
 ];
 
 
