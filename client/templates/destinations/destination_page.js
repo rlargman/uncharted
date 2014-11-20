e170e46227a1e5590fbea0a008a7aecca4547699
Raissa Largman <raissa.largman@gmail.com>
2014-11-19 22:24:18 -0800

    Adding scss, removing destinations list, changing up routing

--- client/templates/destinations/destination_page.js
+++ client/templates/destinations/destination_page.js
@@ -1,11 +1,73 @@
+/** 
+ * Retrieves a single destination from the database that is either the next or
+ * the previous destination from the current one.
+ *
+ * Arguments:
+ * ----------
+ * @param currId - the current destination id
+ * @param sort - ordering to determine if the next or previous destination
+ *  should be retrieved. -1 for previous, 1 for next.
+ *
+ * @return - destination being retrieved
+ */
+var retrieveDestination = function(currId, sort) {
+return Destinations.find({_id: {$gt: currId}}, { 
+      sort: {_id: sort },
+      limit: 1
+  }).fetch()[0];
+}
+
+Template.destinationPage.rendered = function() {
+  $('.destination-main-page').on( "swipe", function(event) { 
+    e.preventDefault();
+
+    var currId = template.data._id;
+    var prevDestination = retrieveDestination(currId, -1);
+    
+    if(!prevDestination) {
+      prevDestination = Destinations.findOne();
+    }
+    
+    Router.go('/destinations/' + prevDestination._id);  
+  });
+}
+
 Template.destinationPage.events({
-  'click .destination-main-page': function(e, template) {
+  /** 
+   * Takes the user to the trip details page when the main page is clicked on.
+   *
+   * Arguments:
+   * ----------
+   * @param e - the event that triggered the handler
+   * @param template - the current template
+   */
+  'click .destination-main-name': function(e, template) {
     e.preventDefault();
     
     var destination = template.data;
-    Router.go(
-      '/destinations/details/' + destination._id, 
-      {destination: destination}
-    );
+    Router.go('/destinations/details/' + destination._id);
+  },
+
+  /** 
+   * Takes the user to previous destination after a swipe left on the main 
+   * page/
+   *
+   * Arguments:
+   * ----------
+   * @param e - the event that triggered the handler
+   * @param template - the current template
+   */
+  'swipe .destination-main-page': function(e, template) {
+    e.preventDefault();
+
+    var currId = template.data._id;
+    var prevDestination = retrieveDestination(currId, -1);
+    
+    if(!prevDestination) {
+      prevDestination = Destinations.findOne();
+    }
+    
+    Router.go('/destinations/' + prevDestination._id);  
   }
 });
+
