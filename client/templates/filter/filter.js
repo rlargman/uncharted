dd6be158a94cbfde241c8969f567934370d7fdb9
Moustafa <masfour@stanford.edu>
2014-11-30 21:16:44 -0800

    added directories and files for filter, wishlists and for wishlists list

---
+++ client/templates/filter/filter.js
@@ -0,0 +1,17 @@
+/** 
+ * Description needed.
+ *
+ * Arguments:
+ * ----------
+ * @param 
+ *
+ * @return
+ */
+
+// button.onclick
+  // if (button is selected)
+    // remove filter from session
+    // deselect button
+  // else
+  	// add filter to active filter in session
+  	// deselect button
