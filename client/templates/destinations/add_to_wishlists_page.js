1f15cb05432e319b6380a1c6e5d4cbac37068766
Moustafa <masfour@stanford.edu>
2014-12-05 16:20:40 -0800

    cancel button now routes back to destination from add_to_wishlists_page template

--- client/templates/destinations/add_to_wishlists_page.js
+++ client/templates/destinations/add_to_wishlists_page.js
@@ -6,7 +6,19 @@ Template.addToWishlistsPage.helpers ({
 });
 
 
-Template.addToWishlistsPage.rendered=function(){
+Template.addToWishlistsPage.rendered = function() {
 	$(".menu-icon").css("display","none");
 	$(".static-menu").css("display","none");
 }
+
+Template.addToWishlistsPage.events({
+  
+  /** 
+   * 
+   */
+  'click .add-to-wishlists-cancel-icon': function(e, template) {
+  	console.log(Session.get('currId'));
+    e.preventDefault();
+    Router.go('/destinations/' + Session.get('currId'));
+  }
+});
